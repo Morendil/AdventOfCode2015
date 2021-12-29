86773ae22b78f230021ed74032643f4145975441
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-29 16:34:26 +0100

    Day 8 part 2

--- Day08.hs
+++ Day08.hs
@@ -1,6 +1,14 @@
 solve1 :: [String] -> Int
 solve1 strings = sum (map length strings) - sum (map decodedLength strings)
 
+solve2 :: [String] -> Int
+solve2 strings = sum (map encodedLength strings) - sum (map length strings)
+
+encodedLength ('"':rest) = 2 + encodedLength rest
+encodedLength ('\\':rest) = 2 + encodedLength rest
+encodedLength (x:rest) = 1 + encodedLength rest
+encodedLength "" = 2
+
 decodedLength ('"':x:rest) = decodedLength (x:rest)
 decodedLength ('\\':'\\':rest) = 1 + decodedLength rest
 decodedLength ('\\':'"':rest) = 1 + decodedLength rest
@@ -12,3 +20,4 @@ decodedLength "" = 0
 main = do
     strings <- lines <$> readFile "day08.txt"
     print $ solve1 strings
+    print $ solve2 strings
