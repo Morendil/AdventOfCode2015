1d27f76f9b7b76d2e078d601485d84d2e73e3cdf
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-30 19:27:47 +0100

    Day 19 part 2

--- Day19.hs
+++ Day19.hs
@@ -1,22 +1,35 @@
-import Data.Text (Text, pack, unpack, splitOn, breakOnAll, drop, concat)
+import Data.Text (Text)
+import qualified Data.Text as T
 import Data.List
+import Data.Ord
+import Data.Tuple (swap)
 
-split :: String -> String -> [String]
-split sep = map unpack . splitOn (pack sep) . pack
+converge :: Eq a => (a -> a) -> a -> a
+converge = until =<< ((==) =<<)
 
-replacement :: String -> (String, String)
+replacement :: Text -> (Text, Text)
 replacement s= (one, two)
-    where [one, _, two] = words s
+    where [one, _, two] = T.words s
 
-substitute :: String -> (String, String) -> [String]
-substitute molecule (from, to) = map perform $ breakOnAll (pack from) (pack molecule)
-    where perform :: (Text, Text) -> String
-          perform (prefix, suffix) = unpack $ Data.Text.concat [prefix, pack to, Data.Text.drop (length from) suffix]
+substitute :: Text -> (Text, Text) -> [Text]
+substitute molecule (from, to) = map perform $ T.breakOnAll from molecule
+    where perform :: (Text, Text) -> Text
+          perform (prefix, suffix) = T.concat [prefix, to, T.drop (T.length from) suffix]
 
-solve1 :: String -> [(String, String)] -> Int
+solve1 :: Text -> [(Text, Text)] -> Int
 solve1 molecule replacements = length $ nub $ concatMap (substitute molecule) replacements
 
+-- greedy reduction - works only if the input is non-adversarial !
+reduce :: [(Text, Text)] -> (Int, Text) -> (Int, Text)
+reduce replacements s = foldl countSteps s replacements
+    where countSteps (n, s) (from, to) = (n+nSteps, T.replace from to s)
+            where nSteps = length $ T.breakOnAll from s
+
+solve2 :: Text -> [(Text, Text)] -> Int
+solve2 molecule replacements = fst $ converge (reduce (map swap replacements)) (0, molecule)
+
 main = do
-    [machine, molecule] <- split "\n\n" <$> readFile "day19.txt"
-    let replacements = map replacement $ lines machine
+    [machine, molecule] <- T.splitOn (T.pack "\n\n") . T.pack <$> readFile "day19.txt"
+    let replacements = map replacement $ T.lines machine
     print $ solve1 molecule replacements
+    print $ solve2 molecule replacements
