0af4280e256f14f0ff9aea36aaed7b0657573e6f
Laurent Bossavit <laurent.git@bossavit.com>
2021-12-31 13:07:44 +0100

    Day 20 part 2

--- Day20.hs
+++ Day20.hs
@@ -3,5 +3,11 @@ import Math.NumberTheory.ArithmeticFunctions ( divisorsList )
 solve1 :: Int -> Int
 solve1 target = head $ filter ((>=target).(*10).sum.divisorsList) [1..]
 
+solve2 :: Int -> Int
+solve2 target = head $ filter ((>=target).(*11).sum.modifiedDivisorsList) [1..]
+
+modifiedDivisorsList n = filter (\d -> d*50 >= n) $ divisorsList n
+
 main = do
     print $ solve1 33100000
+    print $ solve2 33100000
